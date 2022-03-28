d3847412c2c6a15113eb81a48da549bc39f9eeec
moinsoft <mmoin.official@gmail.com>
2022-03-28 17:38:24 +0600

    Create New Four Route

--- index.js
+++ index.js
@@ -13,59 +13,83 @@ app.get('/', (req, res) => {
     
     
   
-  res.send(
+  // res.send(
 
-    `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  //   `
+  //     <!DOCTYPE html>
+  //     <html lang="en">
+  //     <head>
+  //       <meta charset="UTF-8">
+  //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       
-        <title>NodeJS is Awesome</title>
+  //       <title>NodeJS is Awesome</title>
       
-        <style>
+  //       <style>
       
-          *{
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-          }
+  //         *{
+  //           margin: 0;
+  //           padding: 0;
+  //           box-sizing: border-box;
+  //         }
       
-          body{
-            font-family: Arial, Arial, Helvetica, sans-serif;
-            font-size: 1rem;
-          }
+  //         body{
+  //           font-family: Arial, Arial, Helvetica, sans-serif;
+  //           font-size: 1rem;
+  //         }
       
-          .container{
-            width: 50%;
-            margin: 2rem auto;
-            padding: 2rem;
-            background: #dddeee;
-          }
+  //         .container{
+  //           width: 50%;
+  //           margin: 2rem auto;
+  //           padding: 2rem;
+  //           background: #dddeee;
+  //         }
       
-        </style>
+  //       </style>
       
-      </head>
-      <body>
+  //     </head>
+  //     <body>
       
-        <div class="container">
+  //       <div class="container">
       
-          <h1>Hello NodeJS, You Are Awesome</h1>
+  //         <h1>Hello NodeJS, You Are Awesome</h1>
       
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur iste laboriosam id amet atque quaerat cum vel similique praesentium inventore porro cupiditate reprehenderit repudiandae unde earum assumenda, eum eveniet deleniti in et alias. Expedita recusandae nulla dolorum necessitatibus minus cumque soluta quibusdam facilis. Impedit illum nobis harum ab eos!</p>
+  //         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil tenetur iste laboriosam id amet atque quaerat cum vel similique praesentium inventore porro cupiditate reprehenderit repudiandae unde earum assumenda, eum eveniet deleniti in et alias. Expedita recusandae nulla dolorum necessitatibus minus cumque soluta quibusdam facilis. Impedit illum nobis harum ab eos!</p>
       
-        </div>
+  //       </div>
         
-      </body>
-      </html>
+  //     </body>
+  //     </html>
 
-    `
+  //   `
 
-  );
+  // );
+
+  res.send('<h1>This is Root Route.</h1>');
+
+})
 
 
+
+app.get('/about', (req, res) => {
+
+  res.send('<h1>This is About Route.</h1>');
+
+})
+
+
+
+app.get('/products', (req, res) => {
+
+  res.send('<h1>This is Products Route.</h1>');
+
+})
+
+
+
+app.get('/contact', (req, res) => {
+
+  res.send('<h1>This is Contact Route.</h1>');
 
 })
 
