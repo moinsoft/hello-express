b9a3f6c6947b881b3040d5874625c0158a8fbc2f
moinsoft <mmoin.official@gmail.com>
2022-03-29 00:14:31 +0600

    Use of Middleware

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const express = require('express')
-const fs = require('fs')
+const express = require('express');
+const fs = require('fs');
+const cors = require('cors');
+const morgan = require('morgan');
 
-const app = express()
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(cors());
+
 
 
 app.get('/', (req, res) => {
